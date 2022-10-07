78a516792e0d57d33cc1a4be91e61be7a2c81aa0
Deepak Mittal <dpac.mittal2@gmail.com>
2022-10-07 09:43:50 +0530

    Prettier format the code

--- apps/frontend/src/editor/CustomElement.tsx
+++ apps/frontend/src/editor/CustomElement.tsx
@@ -1,36 +1,40 @@
-import React from 'react'
-import { BaseElement } from 'slate'
-import { RenderElementProps } from 'slate-react'
+import React from "react";
+import { BaseElement } from "slate";
+import { RenderElementProps } from "slate-react";
 
 export enum CustomElementType {
-  blockQuote = 'block-quote',
-  bulletedList = 'bulleted-list',
-  headingOne = 'heading-one',
-  headingTwo = 'heading-two',
-  listItem = 'list-item',
-  numberedList = 'numbered-list',
-  paragraph = 'paragraph',
+  blockQuote = "block-quote",
+  bulletedList = "bulleted-list",
+  headingOne = "heading-one",
+  headingTwo = "heading-two",
+  listItem = "list-item",
+  numberedList = "numbered-list",
+  paragraph = "paragraph",
 }
 
 export interface CustomElement extends BaseElement {
-  type: CustomElementType
+  type: CustomElementType;
 }
 
-export const CustomElement: React.FC<RenderElementProps> = ({ attributes, children, element }) => {
+export const CustomElement: React.FC<RenderElementProps> = ({
+  attributes,
+  children,
+  element,
+}) => {
   switch (element.type) {
     case CustomElementType.blockQuote:
-      return <blockquote {...attributes}>{children}</blockquote>
+      return <blockquote {...attributes}>{children}</blockquote>;
     case CustomElementType.bulletedList:
-      return <ul {...attributes}>{children}</ul>
+      return <ul {...attributes}>{children}</ul>;
     case CustomElementType.headingOne:
-      return <h1 {...attributes}>{children}</h1>
+      return <h1 {...attributes}>{children}</h1>;
     case CustomElementType.headingTwo:
-      return <h2 {...attributes}>{children}</h2>
+      return <h2 {...attributes}>{children}</h2>;
     case CustomElementType.listItem:
-      return <li {...attributes}>{children}</li>
+      return <li {...attributes}>{children}</li>;
     case CustomElementType.numberedList:
-      return <ol {...attributes}>{children}</ol>
+      return <ol {...attributes}>{children}</ol>;
     default:
-      return <p {...attributes}>{children}</p>
+      return <p {...attributes}>{children}</p>;
   }
-}
+};
